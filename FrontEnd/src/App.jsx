5e612dca58c11dc0c3c81a714ca70ddbfb9a70c9
SabinGhimireNP <sabinghimire223@gmail.com>
2026-02-22 18:22:33 +0545

    hero aaded

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,21 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './Components/NavBar/NavBar'
+import Home from './Components/home/Home'
 
 function App()  {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<></>} />
-
-        {/* books/sell and books/buy */}
-        <Route path="Books" >
-          <Route index element={<>Books</>} />
-          <Route path="Buy" element={<>Buy Books</>} />
-          <Route path="Sell" element={<>Sell Books</>} />
-        </Route>
-
+        <Route path="/" element={<Home/>} />
          <Route path="Profile" >
           <Route index element={<>Profile</>} />
           <Route path=":id" element={<>data</>} />
